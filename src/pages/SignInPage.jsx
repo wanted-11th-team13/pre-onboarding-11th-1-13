3c88b2d99ddac39911e06f9030e422dd2d5fe072
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-29 02:53:27 +0900

    [Feat] 로그인 페이지 구성

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import AccountForm from '../components/AccountForm';
 
 export default function SignInPage() {
-  return <div>SignInPage</div>;
+  return (
+    <section>
+      <h2>로그인</h2>
+      <AccountForm />
+    </section>
+  );
 }
