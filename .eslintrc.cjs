2411278fa56f19631231c625d093a46f7e817d44
youngeun <jessicak0828@gmail.com>
2023-06-28 14:52:06 +0900

    [Init] 초기세팅

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,6 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
-    node: true,
+    "es6": true
   },
   extends: [
     'eslint:recommended',
@@ -30,7 +28,7 @@ module.exports = {
     'no-var': 'error', // var 금지
     'no-multiple-empty-lines': 'error', // 여러 줄 공백 금지
     'no-console': ['warn', { allow: ['warn', 'error', 'info'] }], // console.log() 금지
-    eqeqeq: 'error', // 일치 연산자 사용 필수
+    'eqeqeq': 'error', // 일치 연산자 사용 필수
     'dot-notation': 'error', // 가능하다면 dot notation 사용
     'no-unused-vars': 'error', // 사용하지 않는 변수 금지
   },
