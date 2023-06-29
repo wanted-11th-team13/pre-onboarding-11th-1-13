964681d3842ae0c1608f97882c52fdd43e62948b
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-29 20:45:26 +0900

    Feat: 투두리스트 페이지 구현

--- src/pages/TodoListPage.jsx
+++ src/pages/TodoListPage.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import AddTodoForm from '../components/AddTodoForm';
+import useSetTodosByResponse from '../hooks/useSetTodosByResponse';
+import Spinner from '../components/Spinner';
+import TodoList from '../components/TodoList';
 
 export default function TodoListPage() {
-  return <div>TodoListPage</div>;
+  const [todos, isLoading, isError, setTodosByResponse] =
+    useSetTodosByResponse();
+
+  if (isError) return <p>인증이 만료되었습니다. 다시 로그인해주세요.</p>;
+  return (
+    <div>
+      <AddTodoForm refetch={setTodosByResponse} />
+      <section>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <TodoList todos={todos} refetch={setTodosByResponse} />
+        )}
+      </section>
+    </div>
+  );
 }
