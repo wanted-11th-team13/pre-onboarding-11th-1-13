c84440c558691297ea7ce8e0c159b9ef0624a154
youngeun <jessicak0828@gmail.com>
2023-06-30 21:06:41 +0900

    [Feat] asset 글꼴 추가, 적용

--- src/components/NormalLi.jsx
+++ src/components/NormalLi.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Containter = styled.div`
+const Container = styled.div`
   display: grid;
   grid-template-columns: 4fr 1fr 1fr;
   align-items: center;
@@ -41,14 +41,10 @@ const Btn = styled.button`
   }
 `;
 
-const Buttons = styled.div`
-  display: flex;
-`;
-
 // eslint-disable-next-line react/prop-types
 function NormalLi({ todo, handleEditMode, handleDelete }) {
   return (
-    <Containter>
+    <Container>
       <p>{todo}</p>
       <Btn onClick={handleEditMode} data-testid="modify-button">
         수정
@@ -56,7 +52,7 @@ function NormalLi({ todo, handleEditMode, handleDelete }) {
       <Btn onClick={handleDelete} data-testid="delete-button">
         삭제
       </Btn>
-    </Containter>
+    </Container>
   );
 }
 
