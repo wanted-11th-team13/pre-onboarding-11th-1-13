8167950b74937ccba6cd3274a6727848797cf8f2
yousunzoo <dpfvmffjqm@gmail.com>
2023-06-30 23:27:02 +0900

    [Feat] 로그아웃 기능 추가

--- src/pages/TodoListPage.jsx
+++ src/pages/TodoListPage.jsx
@@ -5,7 +5,10 @@ import Spinner from '../components/Spinner';
 import TodoList from '../components/TodoList';
 import Background from './components/Background';
 import { styled } from 'styled-components';
-import { Toaster } from 'react-hot-toast';
+import { Toaster, toast } from 'react-hot-toast';
+import logoutIcon from '@/assets/icons/logout.png';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 const Container = styled.div`
   border-radius: 5px;
@@ -15,13 +18,35 @@ const Container = styled.div`
   padding: 10px;
 `;
 
+const Button = styled.button`
+  position: absolute;
+  bottom: 60px;
+  right: 120px;
+  border-radius: 10px;
+  background-color: white;
+  outline: none;
+  border: none;
+`;
+
 export default function TodoListPage() {
   const [todos, isLoading, isError, setTodosByResponse] =
     useSetTodosByResponse();
 
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+    toast('로그아웃되었습니다.');
+    navigate('/');
+  };
+
   if (isError) return <p>인증이 만료되었습니다. 다시 로그인해주세요.</p>;
   return (
     <Background>
+      <Button onClick={handleLogout}>
+        <img src={logoutIcon} alt="logout icon" />
+      </Button>
       <Container>
         <AddTodoForm refetch={setTodosByResponse} />
         <section>
